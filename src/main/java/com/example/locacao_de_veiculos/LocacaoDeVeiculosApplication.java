2d54ca94405454801e637300933441932e99af98
DaniRiber <daniribeirolc@hotmail.com>
2022-05-19 21:42:10 -0300

    arquivos iniciais do spring

---
+++ src/main/java/com/example/locacao_de_veiculos/LocacaoDeVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.example.locacao_de_veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocacaoDeVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocacaoDeVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/locacao_de_veiculos/LocacaoDeVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.locacao_de_veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocacaoDeVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
